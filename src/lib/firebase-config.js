38d4ca11a6591e5de7235a0396c2a6969e74c44c
Solomon Shalom Lijo <solomonlijo@skiff.com>
2025-05-15 00:12:24 +0530

    Jesus W!

--- src/lib/firebase-config.js
+++ src/lib/firebase-config.js
@@ -1,10 +1,10 @@
 export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAhwzpL9tKVV2TgefCV9gZENVyAbghFcjE",
-  authDomain: "bublr-production.firebaseapp.com",
-  projectId: "bublr-production",
-  storageBucket: "bublr-production.firebasestorage.app",
-  messagingSenderId: "957504603256",
-  appId: "1:957504603256:web:cd71352fd2500492ea598d",
+  apiKey: "AIzaSyB5eW6ZEugohsqly7USa5Ia6e-TBUXHr7M",
+  authDomain: "bublr-production-40006.firebaseapp.com",
+  projectId: "bublr-production-40006",
+  storageBucket: "bublr-production-40006.firebasestorage.app",
+  messagingSenderId: "92883700687",
+  appId: "1:92883700687:web:eed767625faa798c14e0ba"
 }
 
 export default FIREBASE_CONFIG
